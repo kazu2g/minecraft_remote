c84c5cd8ea3e02283edb9d10ef54e6f0dd07c8a4
kazu2g <63452221+kazu2g@users.noreply.github.com>
2022-08-10 10:36:59 +0900

    kasdai

--- part2.py
+++ part2.py
@@ -1,14 +1,10 @@
-x=5
+from mcje.minecraft import Minecraft
+import param_MCJE as param
+x=5     
 y=63
 z=10 
-
-
 for a in range(7):
-        from mcje.minecraft import Minecraft
-        import param_MCJE as param
-     
         y+=1
-     
      
         mc=Minecraft.create(port=param.PORT_MC)
         mc.setBlock(x,y,z,  param.GOLD_BLOCK)
--- part5.py
+++ part5.py
@@ -1,18 +1,13 @@
+from mcje.minecraft import Minecraft
+import param_MCJE as param
 x=5
 y=63
 z=10 
 
-
-for i in range(7):
-          
+for i in range(7):     
     for a in range(7):
-        from mcje.minecraft import Minecraft
-        import param_MCJE as param
-     
-        z+=1
-     
-     
         mc=Minecraft.create(port=param.PORT_MC)
         mc.setBlock(x,y,z,  param.GOLD_BLOCK)
+        z+=1
     z=10
         
